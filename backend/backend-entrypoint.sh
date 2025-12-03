2fad2e1bd10713be7c2a8fbaedd50bb7e2b9a92c
Egor Linnikov <98ryba4ok@MacBook-Pro-Egor.local>
2025-12-03 22:37:19 +0300

    fix

--- backend/backend-entrypoint.sh
+++ backend/backend-entrypoint.sh
@@ -1,1 +1,15 @@
-python manage.py migrate && gunicorn presentations_project.wsgi:application --bind 0.0.0.0:8000 --workers 4
+#!/bin/sh
+set -e
+
+echo "⚙️ Applying migrations..."
+python manage.py migrate --noinput
+
+echo "🚀 Starting Gunicorn with stdout logging..."
+
+exec gunicorn presentations_project.wsgi:application \
+    --bind 0.0.0.0:8000 \
+    --workers 4 \
+    --access-logfile '-' \
+    --error-logfile '-' \
+    --capture-output \
+    --log-level debug
